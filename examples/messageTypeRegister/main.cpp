249505e3608d8d410fc921ffeeba34e83cbec6b7
wlgq2 <orcaer@yeah.net>
2018-08-06 22:25:29 +0800

    add some examples...

---
+++ examples/messageTypeRegister/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "MessageType.h"
+
+class ActorTest :public orca::Actor
+{
+public:
+    ActorTest(orca::Framework* framework,std::string name = "")
+        :Actor(framework, name)
+    {
+        registerHandler(std::bind(&ActorTest::handle,this,std::placeholders::_1,std::placeholders::_2));
+    }
+    void handle(orca::MessagePack& pack, orca::Address& from)
+    {
+        std::cout << (char*)(pack.enter()) << std::endl;
+    }
+};
+
+int main(int argc, char** args)
+{
+    //actor framework.
+    orca::Framework framework;
+    //arctor object.
+    ActorTest actor1(&framework);
+    ActorTest actor2(&framework);
+    //message pack.
+    char data[] = "a message of my customize type";
+    orca::MessagePack message;
+    message.create(data,sizeof(data));
+
+    //actor1->actor2 send message.
+    actor1.send(message,actor2.getAddress());
+    framework.loop();
+
+}
---
+++ examples/sendByName/SendByName.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <orca/orca.h>
+
+                                                    
+REGISTER_MESSAGE_TYPE(std::string);
+
+class ActorTest :public orca::Actor
+{
+public:
+    ActorTest(orca::Framework* framework,std::string name = "")
+        :Actor(framework, name)
+    {
+        registerHandler(std::bind(&ActorTest::handle,this,std::placeholders::_1,std::placeholders::_2));
+    }
+    void handle(orca::MessagePack& pack, orca::Address& from)
+    {
+        std::cout << (char*)(pack.enter()) << std::endl;
+    }
+};
+
+int main(int argc, char** args)
+{
+    //actor framework.
+    orca::Framework framework;
+    //arctor object.
+    ActorTest actor1(&framework);
+    ActorTest actor2(&framework,"actor02");
+    //message pack.
+    orca::MessagePack message;
+    message.create("a message to actor2");
+
+    //actor1->actor2 send message.
+    actor1.send(message,"actor02");
+    framework.loop();
+
+}
